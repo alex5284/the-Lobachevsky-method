4c37ee8c8a4dd5adea46c92926c74bbe6c40556f
ark-l <ark-l@ukr.net>
2024-11-16 01:35:10 +0200

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgMatrix = new System.Windows.Forms.DataGridView();
+            this.tbSize = new System.Windows.Forms.TextBox();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.tbH = new System.Windows.Forms.TextBox();
+            this.tbB = new System.Windows.Forms.TextBox();
+            this.tbA = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnAnalitic = new System.Windows.Forms.Button();
+            this.btnLobachevskogo = new System.Windows.Forms.Button();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgMatrix)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgMatrix
+            // 
+            this.dgMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgMatrix.Location = new System.Drawing.Point(12, 12);
+            this.dgMatrix.Name = "dgMatrix";
+            this.dgMatrix.RowHeadersWidth = 51;
+            this.dgMatrix.RowTemplate.Height = 24;
+            this.dgMatrix.Size = new System.Drawing.Size(664, 159);
+            this.dgMatrix.TabIndex = 0;
+            // 
+            // tbSize
+            // 
+            this.tbSize.Location = new System.Drawing.Point(720, 12);
+            this.tbSize.Name = "tbSize";
+            this.tbSize.Size = new System.Drawing.Size(100, 22);
+            this.tbSize.TabIndex = 1;
+            this.tbSize.Text = "4";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(720, 53);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(80, 31);
+            this.btnCreate.TabIndex = 2;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(720, 100);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(80, 31);
+            this.btnTest.TabIndex = 3;
+            this.btnTest.Text = "Test";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // tbH
+            // 
+            this.tbH.Location = new System.Drawing.Point(759, 271);
+            this.tbH.Name = "tbH";
+            this.tbH.Size = new System.Drawing.Size(100, 22);
+            this.tbH.TabIndex = 4;
+            this.tbH.Text = "0,5";
+            // 
+            // tbB
+            // 
+            this.tbB.Location = new System.Drawing.Point(759, 233);
+            this.tbB.Name = "tbB";
+            this.tbB.Size = new System.Drawing.Size(100, 22);
+            this.tbB.TabIndex = 5;
+            this.tbB.Text = "5";
+            // 
+            // tbA
+            // 
+            this.tbA.Location = new System.Drawing.Point(759, 196);
+            this.tbA.Name = "tbA";
+            this.tbA.Size = new System.Drawing.Size(100, 22);
+            this.tbA.TabIndex = 6;
+            this.tbA.Text = "-5";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(703, 271);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 17);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "H = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(704, 233);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 17);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "B = ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(704, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "A = ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(901, 193);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(283, 292);
+            this.listBox1.TabIndex = 16;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(35, 257);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(357, 228);
+            this.listBox2.TabIndex = 17;
+            // 
+            // btnAnalitic
+            // 
+            this.btnAnalitic.Location = new System.Drawing.Point(759, 321);
+            this.btnAnalitic.Name = "btnAnalitic";
+            this.btnAnalitic.Size = new System.Drawing.Size(88, 31);
+            this.btnAnalitic.TabIndex = 18;
+            this.btnAnalitic.Text = "Analitics";
+            this.btnAnalitic.UseVisualStyleBackColor = true;
+            this.btnAnalitic.Click += new System.EventHandler(this.btnAnalitic_Click);
+            // 
+            // btnLobachevskogo
+            // 
+            this.btnLobachevskogo.Location = new System.Drawing.Point(35, 196);
+            this.btnLobachevskogo.Name = "btnLobachevskogo";
+            this.btnLobachevskogo.Size = new System.Drawing.Size(127, 33);
+            this.btnLobachevskogo.TabIndex = 19;
+            this.btnLobachevskogo.Text = "Lobachevskogo";
+            this.btnLobachevskogo.UseVisualStyleBackColor = true;
+            this.btnLobachevskogo.Click += new System.EventHandler(this.btnLobachevskogo_Click);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 16;
+            this.listBox3.Location = new System.Drawing.Point(902, 71);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(283, 100);
+            this.listBox3.TabIndex = 20;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 527);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.btnLobachevskogo);
+            this.Controls.Add(this.btnAnalitic);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbA);
+            this.Controls.Add(this.tbB);
+            this.Controls.Add(this.tbH);
+            this.Controls.Add(this.btnTest);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.tbSize);
+            this.Controls.Add(this.dgMatrix);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgMatrix)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgMatrix;
+        private System.Windows.Forms.TextBox tbSize;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnTest;
+        private System.Windows.Forms.TextBox tbH;
+        private System.Windows.Forms.TextBox tbB;
+        private System.Windows.Forms.TextBox tbA;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnAnalitic;
+        private System.Windows.Forms.Button btnLobachevskogo;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab2
+{
+    public partial class Form1 : Form
+    {
+        int MatrixSize = 0;//розмір многочлена(найбільша сткпінь х)
+        double[] A;//масив коефіцієнтів біля х
+        string f;//задана функція
+        double E, a, b, H;
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void ReadMatrixFromGrid()
+        {
+            A = new double[MatrixSize + 1];//створюємо масив коеф
+
+            for (int i = 0; i < MatrixSize + 1; i++)
+            {
+                A[i] = Convert.ToDouble(dgMatrix.Rows[1].Cells[i].Value);//запам'ятовуємо значення
+            }
+            ShowMatrixInGrid();
+        }
+
+
+        void ShowMatrixInGrid()
+        {
+            for (int i = 0; i < MatrixSize + 1; i++)
+            {
+                dgMatrix.Rows[1].Cells[i].Value = A[i].ToString();//показуємо значення масиву 
+            }
+        }
+
+        void CreateMatrix()//створюємо панелі у DataDridView
+        {
+            MatrixSize = Convert.ToInt32(tbSize.Text);
+            dgMatrix.Rows.Clear();
+            dgMatrix.Columns.Clear(); // очищаем солонки
+            dgMatrix.ColumnCount = MatrixSize + 2;
+            dgMatrix.Rows.Add(2);
+            for (int i = 0; i <= MatrixSize + 1; i++)
+            {
+                if (i < MatrixSize + 1)
+                {
+                    dgMatrix.Rows[0].Cells[i].Value = $" x^{MatrixSize - i}";
+                }
+                else dgMatrix.Rows[0].Cells[i].Value = " y";
+            }
+
+            for (int i = 0; i <= MatrixSize + 1; i++)
+            {
+                if (i < MatrixSize + 1) dgMatrix.Rows[1].Cells[i].Value = "1"; // записываем значеня в соответсвующие места
+                else dgMatrix.Rows[1].Cells[i].Value = "0";
+            }
+        }
+        void CreateMatrix1()
+        {
+            MatrixSize = Convert.ToInt32(tbSize.Text);
+            dgMatrix.Rows.Clear();
+            dgMatrix.Columns.Clear(); // очищаем солонки
+            int j = 0;
+            dgMatrix.ColumnCount = MatrixSize + 2;
+            dgMatrix.Rows.Add(2);
+            for (int i = 0; i <= MatrixSize + 1; i++)
+            {
+                if (i < MatrixSize + 1)
+                {
+                    dgMatrix.Rows[0].Cells[i].Value = $" x^{MatrixSize - i}";
+                }
+                else dgMatrix.Rows[0].Cells[i].Value = " y";
+            }
+
+            j = 1;
+            dgMatrix.Rows[j].Cells[0].Value = "1";
+            dgMatrix.Rows[j].Cells[1].Value = "5";
+            dgMatrix.Rows[j].Cells[2].Value = "9";
+            dgMatrix.Rows[j].Cells[3].Value = "5";
+            dgMatrix.Rows[j].Cells[4].Value = "-1";
+            dgMatrix.Rows[j].Cells[5].Value = "0";
+        }
+        void InitExpr()
+        {
+            E = 0.01;
+            a = Convert.ToDouble(tbA.Text);
+            b = Convert.ToDouble(tbB.Text);
+            H = Convert.ToDouble(tbH.Text);
+
+        }
+
+        void F()//перетворюю масив коеф у рівняння f
+        {
+            A = new double[MatrixSize + 1];
+            ReadMatrixFromGrid();
+            f = " ";
+            for (int i = 0; i < MatrixSize + 1; i++)
+            {
+                f += $"({dgMatrix[i, 1].Value}) * ";
+                if (i < MatrixSize)
+                {
+                    f += $"{dgMatrix[i, 0].Value} + ";
+                }
+                else f += $"{dgMatrix[i, 0].Value}";
+            }
+        }
+        void Afunc()
+        {
+            
+            double X = a;
+            F();
+            MathExpression fun = new MathExpression(f);//берем функцию
+            listBox1.Items.Clear();
+            listBox3.Items.Clear();
+            double previousF = 0;//попередне значення функції
+            double previousX = a;//попередне значення х
+
+            while (X <= b)
+            {
+                double f = fun.Calculate(X);
+
+                if (Math.Sign(f) == 1)
+                {
+                    listBox1.Items.Add("X=" + X.ToString() + ", Sign f(x) = + ");
+                }
+                else
+                {
+                    listBox1.Items.Add("X=" + X.ToString() + ", Sign f(x) = - ");
+                }
+
+                try
+                {//якщо попереднє значення функції не дорівнює нулю та знак значення ф відрізняється від попереднього значення ф
+                    if (previousF != 0 && Math.Sign(previousF) != Math.Sign(f))
+                    {
+                        listBox3.Items.Add(previousX.ToString() + "; " + X.ToString());//записуемо проміжок від попереднього х до даного
+                    }
+                }
+                catch//в іншому випадку проміжків немає
+                {
+                    listBox3.Items.Add("---");
+                }
+
+                if (X != a)//якщо х не дорівнює А
+                {
+                    previousF = f;//попереднє ф прирівнюємо до даного
+                    previousX = X;//попереднє х прирівнюємо до даного
+                }
+
+                X += H;//збільшуємо крок 
+            }
+
+        }
+
+        void Lobachevskogo()
+        {
+            F();
+            MathExpression fun = new MathExpression(f);
+            double X1 = a;
+            listBox2.Items.Clear();
+            A = new double[MatrixSize + 1];
+            ReadMatrixFromGrid();
+            InitExpr();
+            double[] A1 = new double[MatrixSize + 1];
+            double[] A2 = new double[MatrixSize + 1];
+            double[] X = new double[MatrixSize];
+            int k , iter, iter1, k1, k2;
+            double S, res, m, x, tmp, max;
+            for(int i = 0; i < MatrixSize+1; i++)
+            {
+                A1[i] = A[i];
+            }
+            iter = 0;
+            do
+            {
+                k = 0;
+                for(int i = 0; i < MatrixSize+1; i++)
+                {
+                    S = 0;
+                    k1 = i - 1;
+                    k2 = i + 1;
+                    iter1 = 1;
+                    while (true)
+                    {
+                        if (k1 < 0 || k2 > MatrixSize) break;
+                        else
+                        {
+                            if(iter1 % 2 == 0) S = S + 2 * (A1[k1] * A1[k2]);//шукаю проміжні результати для знаходження наступних значень
+                            else S = S - 2 * (A1[k1] * A1[k2]);
+                        }
+
+                        iter1++;
+                        k1 = i - iter1;
+                        k2 = i + iter1;
+                    }
+                    A2[i] = Math.Pow(A1[i], 2) + S;//шукаю по формулам масив значень після піднесення в степінь
+                }
+
+                for(int i = 0; i < MatrixSize+1; i++)
+                {
+                    res = Math.Round(Math.Pow(A1[i], 2),3);
+                    tmp = Math.Round(A2[i],3) - res;
+                    max = Math.Abs(A.Max());
+
+                    if (max < 16)
+                    {
+                        if (Math.Abs(tmp) <= E || iter == 6) k++;//перевіряю умову виходу з ітераційного процессу
+                    }
+                    else
+                    {
+                        if (Math.Abs(tmp) <= E || iter == 5) k++;
+                    }
+                }
+
+                for (int i = 0; i < MatrixSize + 1; i++)
+                {
+                    A1[i] = A2[i];
+                }
+                iter++;
+            } 
+            while (k != MatrixSize+1);
+
+            m = Math.Pow(2, iter);
+
+            for (int i = 0; i <= MatrixSize - 1; i++)
+            {
+                x = -A1[i + 1] / A1[i];//по формулам рахую х
+                if (x < 0) X[i] = (-1) * Math.Pow(Math.Abs(x), 1 / m);
+                else X[i] = Math.Pow(x, 1 / m);
+            }
+            for (int i = 0; i < MatrixSize; i++)//MatrixSize - count
+            {
+                double f = fun.Calculate(X[i]);
+                if (Math.Round(f) != 0) X[i] = X[i] * (-1);
+                f = fun.Calculate(X[i]);
+                if (Math.Round(f) == 0) listBox2.Items.Add("x = " + X[i]);//виводжу х
+            }
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            CreateMatrix1();
+        }
+
+        private void btnLobachevskogo_Click(object sender, EventArgs e)
+        {
+            Lobachevskogo();
+        }
+
+        private void btnAnalitic_Click(object sender, EventArgs e)
+        {
+            InitExpr();
+            Afunc();
+
+        }
+
+        
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            CreateMatrix();
+        }
+
+       
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
